0164cdb702a5053f152cccb3b7259f097ddef3f8
PeteRey <correoernestorey@gmail.com>
2019-06-12 16:05:37 +0200

    End

--- app/src/main/java/com/example/mapbox/MainActivity.java
+++ app/src/main/java/com/example/mapbox/MainActivity.java
@@ -1,60 +1,17 @@
 package com.example.mapbox;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.offline.OfflineManager;
-import com.mapbox.mapboxsdk.offline.OfflineRegion;
-import com.mapbox.mapboxsdk.offline.OfflineRegionError;
-import com.mapbox.mapboxsdk.offline.OfflineRegionStatus;
-import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
-import com.mapbox.mapboxsdk.style.layers.FillLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-
-import org.json.JSONObject;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-
-import timber.log.Timber;
-
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
-    private MapboxMap map;
-    private Button downloadButton;
-    private Button listButton;
-    private Button loadJsonButton;
-
-    private int regionSelected;
-
-    private OfflineManager offlineManager;
-    //private OfflineRegion offlineRegion;
-
-    private static final String TAG = MainActivity.class.toString();
-
-    public static final String JSON_CHARSET = "UTF-8";
-    public static final String JSON_FIELD_REGION_NAME = "FIELD_REGION_NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,236 +26,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap){
-        map = mapboxMap;
-//        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-//                @Override
-//            public void onStyleLoaded(@NonNull Style style) {
-//
-//                offlineManager = OfflineManager.getInstance(MainActivity.this);
-//                downloadButton = findViewById(R.id.downloadButton);
-//                downloadButton.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        downloadRegion();
-//                    }
-//                });
-//
-//                listButton = findViewById(R.id.listButton);
-//                listButton.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        downloadedRegionList();
-//                    }
-//                });
-//
-//                loadJsonButton = findViewById(R.id.JSONbutton);
-//                loadJsonButton.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        mapboxMap.setStyle(new Style.Builder().fromUrl("asset://style-offline.json"));
-//                        Log.e(TAG, "load json");
-//                    }
-//                });
-//            }
-        map.setStyle(Style.LIGHT);
-        loadJsonButton = findViewById(R.id.JSONbutton);
-                loadJsonButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mapboxMap.setStyle(new Style.Builder().fromUrl("asset://style-offline.json"));
-                        Log.e(TAG, "load json");
-                    }
-                });
-        }
-    //}
-
-    public void downloadRegion () {
-
-        String styleUrl = map.getStyle().getUrl();
-        LatLngBounds bounds = map.getProjection().getVisibleRegion().latLngBounds;
-        double minZoom = map.getCameraPosition().zoom;
-        double maxZoom = map.getMaxZoomLevel();
-        float pixelRatio = this.getResources().getDisplayMetrics().density;
-
-        OfflineTilePyramidRegionDefinition definition = new OfflineTilePyramidRegionDefinition(styleUrl, bounds, minZoom, maxZoom, pixelRatio);
-
-
-        // Implementation that uses JSON to store
-        byte[] metadata;
-        try {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put(JSON_FIELD_REGION_NAME, "Campo de Caso");
-            String json = jsonObject.toString();
-            metadata = json.getBytes(JSON_CHARSET);
-        } catch (Exception exception) {
-            Log.e(TAG, "Failed to encode metadata: " + exception.getMessage());
-            metadata = null;
-        }
-
-
-        // Create the region asynchronously
-        offlineManager.createOfflineRegion(definition, metadata,
-                new OfflineManager.CreateOfflineRegionCallback() {
-                    @Override
-                    public void onCreate(OfflineRegion offlineRegion) {
-                        offlineRegion.setDownloadState(OfflineRegion.STATE_ACTIVE);
-
-                        // Monitor the download progress using setObserver
-                        offlineRegion.setObserver(new OfflineRegion.OfflineRegionObserver() {
-                            @Override
-                            public void onStatusChanged(OfflineRegionStatus status) {
-
-                                // Calculate the download percentage
-                                double percentage = status.getRequiredResourceCount() >= 0
-                                        ? (100.0 * status.getCompletedResourceCount() / status.getRequiredResourceCount()) :
-                                        0.0;
-
-                                if (status.isComplete()) {
-                                    // Download complete
-                                    Log.d(TAG, "Region downloaded successfully.");
-                                } else if (status.isRequiredResourceCountPrecise()) {
-                                    String total = String.valueOf(percentage);
-                                    Log.d(TAG, total);
-                                }
-                            }
-
-                            @Override
-                            public void onError(OfflineRegionError error) {
-                                // If an error occurs, print to logcat
-                                Log.e(TAG, "onError reason: " + error.getReason());
-                                Log.e(TAG, "onError message: " + error.getMessage());
-                            }
-
-                            @Override
-                            public void mapboxTileCountLimitExceeded(long limit) {
-                                // Notify if offline region exceeds maximum tile count
-                                Log.e(TAG, "Mapbox tile count limit exceeded: " + limit);
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onError(String error) {
-                        Log.e(TAG, "Error: " + error);
-                    }
-                });
+        mapboxMap.setStyle(new Style.Builder().fromUrl("asset://style_retiro.json"));
     }
-
-
-        private void downloadedRegionList() {
-                // Build a region list
-
-                // Reset the region selected int to 0
-            regionSelected = 0;
-
-                // Query the DB asynchronously
-            offlineManager.listOfflineRegions(new OfflineManager.ListOfflineRegionsCallback() {
-                @Override
-                public void onList(final OfflineRegion[] offlineRegions) {
-                        // Check result. If no regions have been downloaded yet, notify user and return
-                    if (offlineRegions == null || offlineRegions.length == 0) {
-                        Toast.makeText(getApplicationContext(), getString(R.string.toast_no_regions_yet), Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    // Add all of the region names to a list
-                    ArrayList<String> offlineRegionsNames = new ArrayList<>();
-                    for (OfflineRegion offlineRegion : offlineRegions) {
-                        offlineRegionsNames.add(String.valueOf(1));
-                    }
-                    final CharSequence[] items = offlineRegionsNames.toArray(new CharSequence[offlineRegionsNames.size()]);
-
-                    // Build a dialog containing the list of regions
-                    AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
-                            .setTitle(getString(R.string.navigate_title))
-                            .setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                // Track which region the user selects
-                                    regionSelected = which;
-                                }
-                            })
-                            .setPositiveButton(getString(R.string.navigate_positive_button), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id) {
-
-                                    Toast.makeText(MainActivity.this, items[regionSelected], Toast.LENGTH_LONG).show();
-
-                                    // Get the region bounds and zoom
-                                    LatLngBounds bounds = offlineRegions[regionSelected].getDefinition().getBounds();
-                                    double regionZoom = offlineRegions[regionSelected].getDefinition().getMinZoom();
-
-                                    // Create new camera position
-                                    CameraPosition cameraPosition = new CameraPosition.Builder()
-                                            .target(bounds.getCenter())
-                                            .zoom(regionZoom)
-                                            .build();
-
-                                    // Move camera to new position
-                                    map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-                                }
-                            })
-                            .setNeutralButton(getString(R.string.navigate_neutral_button_title), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id) {
-
-                                    // Begin the deletion process
-                                    offlineRegions[regionSelected].delete(new OfflineRegion.OfflineRegionDeleteCallback() {
-                                        @Override
-                                        public void onDelete() {
-                                            Toast.makeText(getApplicationContext(), getString(R.string.toast_region_deleted),
-                                                    Toast.LENGTH_LONG).show();
-                                        }
-
-                                        @Override
-                                        public void onError(String error) {
-                                            Timber.e( "Error: %s", error);
-                                        }
-                                    });
-                                }
-                            })
-                            .setNegativeButton(getString(R.string.navigate_negative_button_title), new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int id) {
-                                // When the user cancels, don't do anything.
-                                // The dialog will automatically close
-                                }
-                            }).create();
-                    dialog.show();
-
-                }
-
-                @Override
-                public void onError(String error) {
-                    Timber.e( "Error: %s", error);
-                }
-            });
-        }
-
-
-
-    public void loadJsonUrl(Style style){
-        try {
-            URL geoJsonUrl = new URL("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_urban_areas.geojson");
-            GeoJsonSource urbanAreasSource = new GeoJsonSource("urban-areas", geoJsonUrl);
-            style.addSource(urbanAreasSource);
-
-            FillLayer urbanArea = new FillLayer("urban-areas-fill", "urban-areas");
-
-            urbanArea.setProperties(
-                    fillColor(Color.parseColor("#ff0088")),
-                    fillOpacity(0.4f)
-            );
-            style.addLayerBelow(urbanArea, "water");
-        } catch (MalformedURLException malformedUrl){
-            malformedUrl.printStackTrace();
-        }
-    }
-
-
-
-    // Add the mapView's own lifecycle methods to the activity's lifecycle methods
+    
     @Override
     public void onStart() {
         super.onStart();
@@ -340,6 +70,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
-
 }
 
