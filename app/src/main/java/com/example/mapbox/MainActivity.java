9360c103e2ed997724634e01967814c704c7d538
PeteRey <correoernestorey@gmail.com>
2019-06-13 18:56:00 +0200

    implementado plugin offline. No descarga de servidor externo

--- app/src/main/java/com/example/mapbox/MainActivity.java
+++ app/src/main/java/com/example/mapbox/MainActivity.java
@@ -3,15 +3,28 @@ package com.example.mapbox;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.offline.OfflineTilePyramidRegionDefinition;
+import com.mapbox.mapboxsdk.plugins.offline.model.NotificationOptions;
+import com.mapbox.mapboxsdk.plugins.offline.model.OfflineDownloadOptions;
+import com.mapbox.mapboxsdk.plugins.offline.offline.OfflinePlugin;
+import com.mapbox.mapboxsdk.plugins.offline.utils.OfflineUtils;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
+    private Button btnDownload;
+    private Style styleUrl;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +39,54 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap){
-        mapboxMap.setStyle(new Style.Builder().fromUrl("asset://style_retiro.json"));
+        mapboxMap.setStyle(new Style.Builder().fromUrl("asset://style_retiro.json"), new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                styleUrl = style;
+                btnDownload = findViewById(R.id.btnDownload);
+                btnDownload.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        downloadRegion();
+                    }
+                });
+            }
+        });
+
     }
-    
+
+    public void downloadRegion(){
+
+        double[] bounds = {40.422710, -3.673897, 40.405392, -3.691835};
+        // Define region of map tiles
+        OfflineTilePyramidRegionDefinition definition = new OfflineTilePyramidRegionDefinition(
+                styleUrl.getUrl(),
+                new LatLngBounds.Builder()
+                        .include(new LatLng(bounds[0], bounds[1]))
+                        .include(new LatLng(bounds[2], bounds[3]))
+                        .build(),
+                14,
+                17,
+                MainActivity.this.getResources().getDisplayMetrics().density
+        );
+
+
+        NotificationOptions notificationOptions = NotificationOptions.builder(this)
+                .smallIconRes(R.drawable.mapbox_logo_icon)
+                .returnActivity(MainActivity.class.getName())
+                .build();
+
+
+        OfflinePlugin.getInstance(this).startDownload(
+                OfflineDownloadOptions.builder()
+                        .definition(definition)
+                        .metadata(OfflineUtils.convertRegionName("retiro"))
+                        .notificationOptions(notificationOptions)
+                        .build()
+        );
+
+    }
+
     @Override
     public void onStart() {
         super.onStart();
